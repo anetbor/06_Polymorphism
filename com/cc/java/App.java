116018bfa38f308932c340dd79c0999527ffb846
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-02-04 07:04:09 +0100

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,15 @@
+package com.cc.java;
+public class App {
+
+    // static Honeybee worker;
+
+    public static void main(String[] args) {
+      
+  
+  
+    }
+
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+}
