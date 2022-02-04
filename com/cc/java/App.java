a2c9e7eedd845714e0aaea6960da8afeb32d0ddd
Anet <anetborin@mail.com>
2022-02-04 11:05:30 +0100

    HoneyBee

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,9 +4,27 @@ public class App {
     // static Honeybee worker;
 
     public static void main(String[] args) {
-      
-  
-  
+     
+        HoneyBee queen =  new Queen();
+        HoneyBee drone = new Drone();
+        HoneyBee worker = new Worker();
+
+         output(queen.doYourJob());
+         output(drone.doYourJob());
+         output(worker.doYourJob());
+
+          output("------------------");
+
+          output(queen.fly());
+          output(drone.fly());
+          output(worker.fly());
+
+          output("---------aber:-------------");
+
+          HoneyBee bee = new HoneyBee();
+          output(bee.doYourJob());
+
+
     }
 
     private static void output(String outputStr) {
